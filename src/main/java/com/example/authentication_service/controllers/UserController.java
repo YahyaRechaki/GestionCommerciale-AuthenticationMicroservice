d79dc1bdf1d601645b36e58f6171a93583603f6c
YahyaRechaki <yahya.rechaki@gmail.com>
2023-01-16 03:57:37 +0100

    Refactoring the register function to avoid unexpected null errors

--- src/main/java/com/example/authentication_service/controllers/UserController.java
+++ src/main/java/com/example/authentication_service/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.example.authentication_service.controllers;
 import com.example.authentication_service.models.User;
 import com.example.authentication_service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -14,9 +16,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+//    @PostMapping("/register")
+//    public User register(@RequestBody User user) {
+//        return userService.createUser(user);
+//    }
+
+    // To avoid Null errors I've modified the register method to return ResponseEntity, and to check if the return value from createUser is not null.
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User newUser = userService.createUser(user);
+        if(newUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 
     @PostMapping("/login")
